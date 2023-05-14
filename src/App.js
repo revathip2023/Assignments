c6987109500904edf0b639ac5b05a30db776f20d
revathip2023 <revathips2023@gmail.com>
2023-05-14 21:37:52 +0530

    version 2 changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Deployment.
+          Github Deployment version 2.
         </p>
         <a
           className="App-link"
